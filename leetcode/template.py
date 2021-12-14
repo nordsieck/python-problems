1a56e7228725bc6976c608afa7552630a8fc2a7b
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2021-12-14 16:06:38 -0500

    Added template for additional problems

---
+++ leetcode/template.py
@@ -0,0 +1,14 @@
+import unittest
+from typing import *
+
+class Solution:
+
+
+class TestSolution(unittest.TestCase):
+    def testFn(self):
+        s = Solution()
+        data = [
+        ]
+
+        for a, b in data:
+            self.assertEqual(s.fn(a), b)
