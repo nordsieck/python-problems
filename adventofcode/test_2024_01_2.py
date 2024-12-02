30aa32e5e76103667760f77ae31a14b694cd89aa
Theo Nordsieck <theo@snthesis.com>
2024-12-01 19:46:29 -0500

    Part 2 of 2024-01 AoC

--- adventofcode/test_2024_01_2.py
+++ adventofcode/test_2024_01_2.py
@@ -1,25 +1,45 @@
+import functools
 import sys
 import unittest
 from typing import *
 
 def main():
     s = Solution()
+    a = []
+    b = {}
 
     with open(sys.argv[1], "r") as file:
         for line in file:
-            pass
+            l = line.split()
+            a.append(int(l[0]))
+            val = int(l[1])
+            if not val in b:
+                b[val] = 1
+            else:
+                b[val] += 1
+
+    print(a)
+    print(b)
+
+    s = Solution()
+    print(s.similarity(a, b))
 
 class Solution:
-    pass
+    def similarity(self, a: List[int], b: Dict[int, int]) -> int:
+        return functools.reduce(lambda acc, e: acc + e * b.get(e, 0), a, 0)
 
 class TestSolution(unittest.TestCase):
     def testFn(self):
         s = Solution()
         data = [
+            [[1], {1:1}, 1],
+            [[1, 1], {1:1}, 2],
+            [[1], {2:1}, 0],
+            [[3, 4, 2, 1, 3, 3], {3:3, 4:1, 5:1, 9:1}, 31],
         ]
 
-        for a, b in data:
-            self.assertEqual(s.fn(a), b)
+        for a, b, sim in data:
+            self.assertEqual(s.similarity(a, b), sim)
 
 if __name__ == "__main__":
     main()
